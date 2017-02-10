62a88256588e93f5be0aef9f329e57f87600fd2f
sxf <sunxfancy@gmail.com>
2017-02-10 15:24:24 +0800

    add generator
    Signed-off-by: sxf <sunxfancy@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -4,26 +4,28 @@ class LibCharsetDetectConan(ConanFile):
     name = "libcharsetdetect"
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
     # No exports_sources necessary
 
     def source(self):
         # this will create a hello subfolder, take it into account
         self.run("git clone https://github.com/elite-lang/libcharsetdetect.git")
-        # conan_magic_lines = '''PROJECT(charsetdetect)
-        # include(../conanbuildinfo.cmake)
-        # CONAN_BASIC_SETUP()
-        # '''
-        # tools.replace_in_file("libcharsetdetect/CMakeLists.txt", "PROJECT(charsetdetect)", conan_magic_lines)
+        conan_magic_lines = '''PROJECT(charsetdetect)
+        include(../conanbuildinfo.cmake)
+        CONAN_BASIC_SETUP()
+        '''
+        tools.replace_in_file("libcharsetdetect/CMakeLists.txt", "PROJECT(charsetdetect)", conan_magic_lines)
 
     def build(self):
         cmake = CMake(self.settings)
+        print 'cmake %s/libcharsetdetect %s' % (self.conanfile_directory, cmake.command_line)
         self.run('cmake %s/libcharsetdetect %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="libcharsetdetect")
-        self.copy("*.lib", dst="lib", src="build")
-        self.copy("*.a", dst="lib", src="build")
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
 
     def package_info(self):
         self.cpp_info.libs = ["charsetdetect"]
