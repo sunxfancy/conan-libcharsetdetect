f1501ac24f459415ae730e7ac182f00f18920439
sxf <sunxfancy@gmail.com>
2017-02-10 16:22:25 +0800

    add readme change url and license
    Signed-off-by: sxf <sunxfancy@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,11 @@ from conans import ConanFile, CMake, tools
 class LibCharsetDetectConan(ConanFile):
     name = "libcharsetdetect"
     version = "1.0"
+    license = "LGPL"
+    url = "https://github.com/sunxfancy/conan-libcharsetdetect"
     settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     generators = "cmake"
     # No exports_sources necessary
 
@@ -18,14 +22,16 @@ class LibCharsetDetectConan(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        print 'cmake %s/libcharsetdetect %s' % (self.conanfile_directory, cmake.command_line)
-        self.run('cmake %s/libcharsetdetect %s' % (self.conanfile_directory, cmake.command_line))
+        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+        self.run('cmake %s/libcharsetdetect %s %s' % (self.conanfile_directory, cmake.command_line, shared))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="libcharsetdetect")
-        self.copy("*.lib", dst="lib", src="lib")
-        self.copy("*.a", dst="lib", src="lib")
-
+        self.copy("*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy("*.a", dst="lib", src="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", src="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", src="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", src="lib", keep_path=False)
     def package_info(self):
         self.cpp_info.libs = ["charsetdetect"]
