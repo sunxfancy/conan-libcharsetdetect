719dadf22a34317f7fa865d4c2d6f57b7ac783a7
西风逍遥游 <sun_xfancy@163.com>
2017-11-17 01:16:19 -0800

    fix

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class LibCharsetDetectConan(ConanFile):
     name = "libcharsetdetect"
-    version = "1.0"
+    version = "1.1"
     license = "LGPL"
     url = "https://github.com/sunxfancy/conan-libcharsetdetect"
     settings = "os", "compiler", "build_type", "arch"
@@ -21,16 +21,16 @@ class LibCharsetDetectConan(ConanFile):
         include(../conanbuildinfo.cmake)
         CONAN_BASIC_SETUP()
         '''
-        tools.replace_in_file("libcharsetdetect/CMakeLists.txt", "PROJECT(charsetdetect)", conan_magic_lines)
+        tools.replace_in_file("libcharsetdetect-master/CMakeLists.txt", "PROJECT(charsetdetect)", conan_magic_lines)
 
     def build(self):
         cmake = CMake(self.settings)
         shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        self.run('cmake %s/libcharsetdetect %s %s' % (self.conanfile_directory, cmake.command_line, shared))
+        self.run('cmake %s/libcharsetdetect-master %s %s' % (self.conanfile_directory, cmake.command_line, shared))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="libcharsetdetect")
+        self.copy("*.h", dst="include", src="libcharsetdetect-master")
         self.copy("*.lib", dst="lib", src="lib", keep_path=False)
         self.copy("*.a", dst="lib", src="lib", keep_path=False)
         self.copy("*.dll", dst="bin", src="lib", keep_path=False)
